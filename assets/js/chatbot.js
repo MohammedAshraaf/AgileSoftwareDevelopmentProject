14195fed4a6d736deba4210ffe1a7cf96cddc38a
Mohamed Ashraf <mohammed.ashraaf.15@gmail.com>
2022-02-12 23:40:42 +0200

    Add the code for the azure healthbot integration

--- assets/js/chatbot.js
+++ assets/js/chatbot.js
@@ -1,22 +1,29 @@
-// Make chatbot open by default.
-var isChatOpen = true;
-
-/* Open and closes the chatbot. */
-$('#chat-button button').click(function () {
-  if (isChatOpen) {
+// Make chatbot closed by default.
+var isChatOpen = false;
+var isBotCreated = false;
+$('#chat-button button').click(function(){
+  if(isChatOpen)
+  {
     // Close chatbot.
-    $('#chatbox').css('display', 'none');
-    $('#chatbot').css('height', '32px');    
+    $('#webchat').css('display', 'none');
+    $('#chatbot').css('height', '32px'); 
     // Update boolean to reflect that chatbot is now closed.
-    isChatOpen = false;
+    isChatOpen = false;  
   }
-  else {
+  else
+  {
     // Open chatbot.
-    $('#chatbox').css('display', 'flex');
+    $('#webchat').css('display', 'block');
+    isChatOpen = true;
     increaseChatHeight();
     // Update boolean to reflect that chatbot is now open.
-    isChatOpen = true;
+    if(!isBotCreated)
+    {
+      chatRequested();
+      isBotCreated = true
+    }
   }
+    
 });
 
 /* Sets the chatbot height when the window is resized. */
@@ -27,8 +34,15 @@ $(window).resize(function () {
   increaseChatHeight();
 });
 
+
 /* Displays the chatbot by setting its height for different screen sizes. */
 function increaseChatHeight() {
+  if(!isChatOpen)
+  {
+    $('#chatbot').css('height', '32px'); 
+    console.log("hello")
+    return;
+  }
   if (window.matchMedia('(max-width: 767px)').matches) {
     $('#chatbot').css('height', "22em");
   }
