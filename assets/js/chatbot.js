79e813f33f98df9bc2e09ae4c06211cf4dbb935a
Mohamed Ashraf <mohammed.ashraaf.15@gmail.com>
2022-02-13 22:10:17 +0200

    update the chatbox style and add chat icon

--- assets/js/chatbot.js
+++ assets/js/chatbot.js
@@ -1,12 +1,17 @@
 // Make chatbot closed by default.
 var isChatOpen = false;
 var isBotCreated = false;
-$('#chat-button button').click(function(){
+$('#chat-button').click(function(){
   if(isChatOpen)
   {
     // Close chatbot.
     $('#webchat').css('display', 'none');
     $('#chatbot').css('height', '32px'); 
+    $('#chatbot').css('border', 'none'); 
+    $('#chatbot').css('box-shadow', 'none'); 
+
+    $("#chat-button").css("bottom", "1.5em");
+
     // Update boolean to reflect that chatbot is now closed.
     isChatOpen = false;  
   }
@@ -40,16 +45,33 @@ function increaseChatHeight() {
   if(!isChatOpen)
   {
     $('#chatbot').css('height', '32px'); 
-    console.log("hello")
+    $('#chatbot').css('border', 'none'); 
+    $('#chatbot').css('box-shadow', 'none'); 
+    $("#chat-button").css("bottom", "1.5em");
     return;
   }
   if (window.matchMedia('(max-width: 767px)').matches) {
     $('#chatbot').css('height', "22em");
+    $("#chat-button").css("bottom", "21.5em");
+    $('#chatbot').css('border', '2px solid black'); 
+    $('#chatbot').css('box-shadow', '0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28)'); 
+
+
   }
   else if (window.matchMedia('(max-width: 992px)').matches) {
     $('#chatbot').css('height', "27em");
+    $("#chat-button").css("bottom", "26.5em");
+    $('#chatbot').css('border', '2px solid black'); 
+    $('#chatbot').css('box-shadow', '0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28)'); 
+
+
   }
   else {
     $('#chatbot').css('height', "37em");
+    $("#chat-button").css("bottom", "36.5em");
+    $('#chatbot').css('border', '2px solid black'); 
+    $('#chatbot').css('box-shadow', '0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28)'); 
+
+
   }
 }
